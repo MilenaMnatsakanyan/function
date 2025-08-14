d50afe585a84d1830152c17c2f67c85da03ccd4a
MilenaMnatsakanyan <mnatsakanyanmilena87@gmail.com>
2025-08-14 16:11:09 +0400

    recursive function print array

---
+++ printarr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define SIZE 5
+void print(int arr[],int  size){
+	if(size > 0){
+		print(arr, size - 1);
+		printf("%d",arr[size - 1]);		
+	}
+}
+int main() {
+	int arr[] = {1,2, 3, 4, 5};
+	print(arr,SIZE);
+	return 0;
+	}
+
